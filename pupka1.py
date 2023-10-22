6bdd5c8a60446f3ef0724b7374b0f03f2a57bdcc
olesxlkk <143779619+olesxlkk@users.noreply.github.com>
2023-10-22 20:54:32 +0700

    a

---
+++ pupka1.py
@@ -0,0 +1,19 @@
+s = input('enter str: ')
+
+print(s[2])
+
+print(s[-2])
+            
+print(s[0:5])
+
+print(s[0:-2])
+
+print(s[::2])
+
+print(s[1::2])
+
+print(s[::-1])
+
+print(s[::-2])
+
+print(len(s))
